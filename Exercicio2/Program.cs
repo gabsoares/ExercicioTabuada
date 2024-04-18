461352c6f82dd03159a7562a2a323d0e88667067
gabsoares <soaresgabriel68@gmail.com>
2024-04-18 17:36:54 -0300

    Add project files.

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Digite o numero que vc deseja saber a tabuada: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
+{
+    if (num > 0)
+    {
+        int mult = num * i;
+        Console.WriteLine($"{num}x{i} = {mult}");
+    }
+}
